b25e2a73c096eace0dbba425efc2333370cfb570
Dacriper <cristian.pereyra@mi.unc.edu.ar>
2020-06-21 20:20:25 -0300

    Lector con archivos.

--- Codigo/Clases/src/clases/ArchivoTop10.java
+++ Codigo/Clases/src/clases/ArchivoTop10.java
@@ -1,9 +1,52 @@
 package clases;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class ArchivoTop10 implements Lector {
+    private TreeMap<Integer, String> Top10;
+    private BufferedReader BR;
+    private String Linea;
 
-	public ArchivoTop10() {
-		// TODO Auto-generated constructor stub
-	}
+    ArchivoTop10() throws IOException {
+        FileReader arc = new FileReader("C:\\top10.txt");
+        this.BR = new BufferedReader(arc);
+        this.Top10 = new TreeMap<Integer, String>();
+    }
+    
+    @Override
+    public void guardarLinea(int puntaje, String palabra) {
+        Top10.put(puntaje, palabra);
+    }
 
+    public void guardarMapATexto(Map top10){
+
+    }
+
+    @Override
+    public void leerArchivo () throws IOException {
+    	try {
+    			StreamTokenizer st = new StreamTokenizer(BR);
+    			while (st.nextToken() != StreamTokenizer.TT_EOF) {
+    				guardarLinea((int)st.nval, st.sval);
+            }
+        } catch (Exception e) {
+        		e.printStackTrace();
+        } finally {
+            try {
+                	if (null != BR) {
+                		BR.close();
+                	}
+            	} catch (Exception e2) {
+            		e2.printStackTrace();
+            	}
+        }
+   }
+
+    public TreeMap getMap () {
+        return Top10;
+    }
 }
--- Codigo/Clases/src/clases/Lector.java
+++ Codigo/Clases/src/clases/Lector.java
@@ -1,5 +1,9 @@
 package clases;
 
+import java.io.IOException;
+
 public interface Lector {
 
+    void leerArchivo() throws IOException;
+    void guardarLinea(int i, String s);
 }
